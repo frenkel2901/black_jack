0c5fef7cc77199028fad3cb06e445436175af8bd
frenkel2901 <a.frenkel2901@gmail.com>
2022-10-29 18:48:05 +0300

    add deck

---
+++ Game/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+
+  ALL_CARDS = [ 2, 3, 4, 5, 6 ,7 ,8 ,9, 10, 'J', 'Q', 'K', 'A' ].freeze
+  SUITS = [ '+', '<3', '^', '<>' ].freeze
+
+  attr_reader :given
+
+  def give_card
+    @given ||= []
+    @given_now = "#{ALL_CARDS.sample}#{SUITS.sample}"
+    gived?
+    @given << @given_now
+    puts @given_now
+  end
+
+  private
+
+  def gived?
+    raise "Already given!" if @given.include?(@given_now)
+  end
+end
