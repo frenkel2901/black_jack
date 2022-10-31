44efb7ac285afa10e901d2920ae069d906c78ad5
frenkel2901 <a.frenkel2901@gmail.com>
2022-10-31 13:07:35 +0300

    fixed card_counter.rb

--- Game/deck.rb
+++ Game/deck.rb
@@ -1,6 +1,4 @@
-ALL_CARDS = [ 2, 3, 
-  #4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 
-  'K', 'A'].freeze
+ALL_CARDS = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
 SUITS = ['♧', '♡', '♤', '♢'].freeze
 
 class Deck
